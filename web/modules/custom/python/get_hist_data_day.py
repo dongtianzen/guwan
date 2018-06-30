8807b6b0eea3c47f2901d60ccdd3e83d5711f345
dongtianzen <dongtianzen@gmail.com>
2018-06-30 14:08:13 -0400

    dev date, timedelta

--- web/modules/custom/python/get_hist_data_day.py
+++ web/modules/custom/python/get_hist_data_day.py
@@ -4,17 +4,21 @@
 # python3 get_hist_data_day.py
 
 import tushare as ts
+from datetime import date, timedelta
 
 # alternative ts.get_k_data
 # cc = ts.get_k_data('399300', index=True,start='2016-10-01', end='2017-01-31')
 # print (cc)
 
+# todayDate is like '2017-12-26'
+todayDate = str(date.today())
+yesterday = str(date.today() - timedelta(1))
+
+print (yesterday)
+
 # get Day data
 code = '000875'
-df = ts.get_hist_data(code, ktype='D')
-
-# get 60 m data
-# df = ts.get_hist_data(code, ktype='60')
+df = ts.get_hist_data(code, ktype = 'D', start = yesterday)
 
 # 生成Json格式的文件
 df.to_json('000875_60m.json', orient='index')

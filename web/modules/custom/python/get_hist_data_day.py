94a1a947bd3319e16b957df02ebe47259bae681a
dongtianzen <dongtianzen@gmail.com>
2018-07-01 07:03:10 -0400

    dev generateHistoryDataToJson

--- web/modules/custom/python/get_hist_data_day.py
+++ web/modules/custom/python/get_hist_data_day.py
@@ -3,12 +3,13 @@
 # cd web/modules/custom/python
 # python3 get_hist_data_day.py
 
-import tushare as ts
-from datetime import date, timedelta
-
 # alternative ts.get_k_data
 # cc = ts.get_k_data('399300', index=True, start='2016-10-01', end='2017-01-31')
 # print (cc)
+
+#
+import tushare as ts
+from datetime import date, timedelta
 
 # todayDate is like '2017-12-26'
 todayDate = str(date.today())
@@ -21,18 +22,18 @@ def getHistoryData(code):
   histData = ts.get_hist_data(code, ktype = 'D', start = yesterday)
   return histData
 
-
 histData = getHistoryData(code)
-
-
-print (histData)
-exit()
 
 for row in histData.index.values:
   histDataCache = histData.rename(index={row: (code + '_' + row)})
   histData = histDataCache
 
+# debug
+# print (histData)
+# exit()
 
 # 生成Json格式的文件
 def generateHistoryDataToJson():
   histData.to_json('historyDataDat.json', orient='index')
+
+generateHistoryDataToJson()

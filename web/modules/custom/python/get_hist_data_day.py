168b1af26cd69c7d705897acf9b24a013c2d39f3
dongtianzen <dongtianzen@gmail.com>
2018-07-16 23:36:40 -0400

    dev specifyCodeListWithPageNum

--- web/modules/custom/python/get_hist_data_day.py
+++ web/modules/custom/python/get_hist_data_day.py
@@ -24,8 +24,6 @@ start_time = time.time()
 class RunGetHistData:
 
   def specifyCodeList(self):
-    # codeList = FlexJsonBasic().convertViewsJsonToTermCodeList()
-
     # codeList = ['600006', '600007', '600008', '600009', '600010']
     codeList = ['600290', '600291']
 
@@ -38,9 +36,24 @@ class RunGetHistData:
     jsonFilePath = FlexJsonBasic().getGenerateJsonFilePath(fileName)
     FlexJsonBasic().generateJsonFromData(jsonFilePath, allHistoryData)
 
+  def specifyCodeListWithPageNum(self, pageNum):
+    codeList = FlexJsonBasic().convertViewsJsonToTermCodeList(pageNum)
+
+    # startDate is today('2018-06-23') 减去 想开始的日期个数
+    startDate = str(date.today() - timedelta(2))
+
+    allHistoryData = FlexTushareBasic().downloadHistDataByCode(codeList, startDate)
+
+    fileName = 'historyDataByCodeListPiece_' + str(pageNum) + '.json'
+    jsonFilePath = FlexJsonBasic().getGenerateJsonFilePath(fileName)
+    FlexJsonBasic().generateJsonFromData(jsonFilePath, allHistoryData)
+
   # -->
 
-RunGetHistData().specifyCodeList()
+
+for pageNum in range(6):
+  print(pageNum)
+  RunGetHistData().specifyCodeListWithPageNum(pageNum)
 
 # for print execution time end
 print("--- %s seconds ---" % (time.time() - start_time))

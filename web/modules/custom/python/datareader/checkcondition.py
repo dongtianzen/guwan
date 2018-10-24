12b25dc8859a3b46f6ee307efbedb45f339ff691
dongtianzen <dongtianzen@gmail.com>
2018-10-23 23:26:34 -0400

    dev getSumPrice

--- web/modules/custom/python/datareader/checkcondition.py
+++ web/modules/custom/python/datareader/checkcondition.py
@@ -7,28 +7,38 @@ python3 web/modules/custom/python/datareader/checkcondition.py
 
 #%%
 # define a class
-class checkCondition:
+class CheckCondition:
+
+  #
+  def getSumPrice(self, pricesDf, key, num = 5):
+    output = 0
+
+    maxNum = (key - num)
+    while (key > maxNum):
+
+      # @param 3 is Close price
+      print(pricesDf.iat[key, 3])
+      output += pricesDf.iat[key, 3]
+      key = key - 1
+
+    return output
 
   #
   def getAveragePrice(self, pricesDf, key, num = 5):
+    output = 0
 
+    sumPrice = self.getSumPrice(pricesDf, key, num)
+    if (num > 0):
+      output = sumPrice / num
+
+    return output
 
   # compare ma5 ma10 on the min < (ma5/ma10) < max
   # @return Boolean, true or false
-  def comparePriceRatio(self):
+  def comparePriceRatio(self, pricesDf, key):
     priceMa5  = self.getAveragePrice(pricesDf, key, num = 5);
+    print(priceMa5)
 
-    priceMa10 = self.getAveragePrice(pricesDf, key, num = 10);
+    # priceMa10 = self.getAveragePrice(pricesDf, key, num = 10);
+    # print(priceMa5)
 
-    if ($price_ma10) {
-      $price_ratio = \Drupal::getContainer()
-        ->get('flexinfo.calc.service')
-        ->getPercentage($price_ma5, $price_ma10);
-
-      if ($min < $price_ratio && $price_ratio < $max) {
-        $output = TRUE;
-      }
-      else {
-      }
-    }
-    start = datetime.datetime(2018, 10, 18)
--- web/modules/custom/python/datareader/runstockrank.py
+++ web/modules/custom/python/datareader/runstockrank.py
@@ -5,19 +5,24 @@ python3 web/modules/custom/python/datareader/runstockrank.py
 """
 
 from getstockdata import GetPriceBasic
+from checkcondition import CheckCondition
 
 pricesDf = GetPriceBasic().getHistPrice()
-print(pricesDf.info())
+# print(pricesDf.info())
 
-# print(pricesDf)
+print(pricesDf['Close'])
+print(pricesDf['Close'][-1])
+
+# CheckCondition().comparePriceRatio(pricesDf, -1)
+
 
 # 选取DataFrame最后一行，返回的是DataFrame
-print(pricesDf.iloc[-1:])
+# print(pricesDf.iloc[-1:])
 
 #选取最后一行, 第5列，用于已知行、列位置的选
 # Date        High   Low    Open   Close  Volume   Adj Close
 # 2018-10-24  23.07  22.62  22.65  22.79  3593873  22.790001
-print(pricesDf.iat[-1, 3])
+# print(pricesDf.iat[-1, 3])
 # print(pricesDf['2018-10-24':])
 # print(pricesDf.loc['2018-10-24',['Close']])
 # print(pricesDf['2018-10-24', 'Close'])
